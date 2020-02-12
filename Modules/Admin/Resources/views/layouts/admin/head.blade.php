105ce9b0a03b5b847d8053e05d95cd340c890c44
thuthuy2610 <thuypham12049@gmail.com>
2020-02-12 09:14:40 +0700

    [Update]Frontend Admin

--- Modules/Admin/Resources/views/layouts/admin/head.blade.php
+++ Modules/Admin/Resources/views/layouts/admin/head.blade.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge, width=device-width, initial-scale=1.0">
-    <title>Barhop</title>
+    <title>Blog</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Font Awesome -->
--- Modules/Admin/Resources/views/login.blade.php
+++ Modules/Admin/Resources/views/login.blade.php
@@ -4,7 +4,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
     <div class="login-logo">
-        <a href="../../index2.html"><b>Barhop</b></a>
+        <a href="../../index2.html"><b>Blog</b></a>
     </div>
     <!-- /.login-logo -->
     <div class="card">
